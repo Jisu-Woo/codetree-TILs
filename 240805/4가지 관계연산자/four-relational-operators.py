7735cc652e2422ad8acbd879803273cdcdc27d56
Jisu <162106530+Jisu-Woo@users.noreply.github.com>
2024-08-05 21:44:02 +0900

    Title: 4가지 관계연산자, Time: 120ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 240805/4가지 관계연산자/four-relational-operators.py
@@ -0,0 +1,24 @@
+z = input().split()
+
+a = int(z[0])
+b = int(z[1])
+
+if a>=b:
+    print(1)
+else:
+    print(0)
+
+if a>b:
+    print(1)
+else:
+    print(0)
+
+if a<=b:
+    print(1)
+else:
+    print(0)
+
+if a<b:
+    print(1)
+else:
+    print(0)
