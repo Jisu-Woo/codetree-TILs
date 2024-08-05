aac671820b2fcad6c99512492f3954e3063a42d3
Jisu <162106530+Jisu-Woo@users.noreply.github.com>
2024-08-05 21:42:32 +0900

    Title: 비교 연산, Time: 120ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 240805/비교 연산/comparison-operator.py
@@ -0,0 +1,28 @@
+z = input().split()
+a = int(z[0])
+b = int(z[1])
+
+if a>=b:
+    print(1)
+else:
+    print(0)
+if a>b:
+    print(1)
+else:
+    print(0)
+if a<=b:
+    print(1)
+else:
+    print(0)
+if a<b:
+    print(1)
+else:
+    print(0)
+if a==b:
+    print(1)
+else:
+    print(0)
+if a!=b:
+    print(1)
+else:
+    print(0)
