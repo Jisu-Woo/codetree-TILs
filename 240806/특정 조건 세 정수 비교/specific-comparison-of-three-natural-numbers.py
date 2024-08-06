3213bb42213952c8264acda2350c368d73c4ed83
Jisu <162106530+Jisu-Woo@users.noreply.github.com>
2024-08-06 21:40:35 +0900

    Title: 특정 조건 세 정수 비교, Time: 139ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240806/특정 조건 세 정수 비교/specific-comparison-of-three-natural-numbers.py
@@ -0,0 +1,15 @@
+z = input().split()
+
+a = int(z[0])
+b = int(z[1])
+c = int(z[2])
+
+if a <= b and a <= c:
+    print(1)
+else:
+    print(0)
+
+if a == b and b == c:
+    print(1)
+else:
+    print(0)
