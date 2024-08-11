7c678ad42b23c875343218dcf937c81d735b52f8
Jisu <162106530+Jisu-Woo@users.noreply.github.com>
2024-08-11 21:56:50 +0900

    Title: 몫 출력하기, Time: 121ms, Memory: 24MB, Status: Passed - CodeTree

--- 240811/몫 출력하기/print-share.py
+++ 240811/몫 출력하기/print-share.py
@@ -3,7 +3,8 @@ while True:
     n = int(input())
 
     if n % 2 == 0:
-        if cnt >= 3:
-            break
         print(n//2)
         cnt += 1
+
+        if cnt >= 3: # cnt 체크를 뒤에 해줘야 while문이 한번 더 돌지 않게 됨! (입력을 한 번 더 받게 되는 것을 방지)
+            break
