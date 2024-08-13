0d1c2e0e2210e6b54c26600834528dd8bd277865
Jisu <162106530+Jisu-Woo@users.noreply.github.com>
2024-08-13 21:29:45 +0900

    Title: ab 사이에 없는 c, Time: 112ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 240813/ab 사이에 없는 c/c-between-a-and-b-2.py
@@ -0,0 +1,16 @@
+z = input().split()
+
+a = int(z[0])
+b = int(z[1])
+c = int(z[2])
+
+
+is_exist = False
+for i in range(a, b + 1):
+    if i % c == 0:
+        is_exist = True
+
+if is_exist:
+    print("NO")
+else:
+    print("YES")
