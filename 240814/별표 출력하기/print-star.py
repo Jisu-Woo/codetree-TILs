c58bd68118b77004a6b9f91b7d99f08cb5f063a2
Jisu <162106530+Jisu-Woo@users.noreply.github.com>
2024-08-14 20:27:47 +0900

    Title: 별표 출력하기, Time: 102ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240814/별표 출력하기/print-star.py
@@ -0,0 +1,11 @@
+n = int(input())
+
+for i in range(5):
+    for j in range(i + 1):
+        print("*", end=" ")
+    print()
+
+for i in range(4):
+    for j in range(0, 4 - i):
+        print("*", end=" ")
+    print()
