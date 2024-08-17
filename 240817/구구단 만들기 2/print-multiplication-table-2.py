b53c672d054537fbf847e12fe6021272f45e87d9
Jisu <162106530+Jisu-Woo@users.noreply.github.com>
2024-08-17 12:32:25 +0900

    Title: 구구단 만들기 2, Time: 115ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 240817/구구단 만들기 2/print-multiplication-table-2.py
@@ -0,0 +1,10 @@
+z = input().split()
+a = int(z[0])
+b = int(z[1])
+
+for i in range(2, 9, 2):
+    for j in range(b, a - 1, -1):
+        print(f"{j} * {i} = {i*j}", end=" ")
+        if j > a:
+            print("/", end=" ")
+    print()
