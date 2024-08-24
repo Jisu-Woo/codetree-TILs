313fe9d8c976c342eda961b3b022ee054343df6e
Jisu <162106530+Jisu-Woo@users.noreply.github.com>
2024-08-24 21:28:12 +0900

    Title: 재귀함수를 이용한 최소공배수, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240824/재귀함수를 이용한 최소공배수/least-common-multiple-using-recursive-function.py
@@ -0,0 +1,12 @@
+n = int(input())
+
+arr = list(map(int, input().split()))
+
+for i in range(1, 10000):
+    is_True = True
+    for j in range(6):
+        if i % arr[j] != 0:
+            is_True = False
+    if is_True:
+        print(i)
+        break
