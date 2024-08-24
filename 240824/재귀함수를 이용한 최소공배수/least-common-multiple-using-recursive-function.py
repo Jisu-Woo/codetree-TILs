5c39cd7ee1e932ec9c0d3f5711a12d19dc8a86ed
Jisu <162106530+Jisu-Woo@users.noreply.github.com>
2024-08-24 21:36:25 +0900

    Title: 재귀함수를 이용한 최소공배수, Time: 165ms, Memory: 25MB, Status: Passed - CodeTree

--- 240824/재귀함수를 이용한 최소공배수/least-common-multiple-using-recursive-function.py
+++ 240824/재귀함수를 이용한 최소공배수/least-common-multiple-using-recursive-function.py
@@ -2,11 +2,23 @@ n = int(input())
 
 arr = list(map(int, input().split()))
 
+def ret(i, j):
+    if j >= n:
+        return True
+    if i % arr[j] == 0:
+        return ret(i, j + 1)
+    else:
+        return False
+
+
+
+
 for i in range(1, 10000):
-    is_True = True
-    for j in range(n):
-        if i % arr[j] != 0:
-            is_True = False
+    is_True = ret(i, 0)
+    #for j in range(n):
+    #    ret(i, 0)
+    #    if i % arr[j] != 0:
+    #        is_True = False
     if is_True:
         print(i)
         break
