5c6ec206e304b862ae5037d20ccb9463e5c123f9
Jisu <162106530+Jisu-Woo@users.noreply.github.com>
2024-08-25 23:02:00 +0900

    Title: 문자에 따른 명령 2, Time: 102ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240825/문자에 따른 명령 2/text-based-commands2.py
+++ 240825/문자에 따른 명령 2/text-based-commands2.py
@@ -11,7 +11,7 @@ for i in range(len(str)):
     elif str[i] == 'R':
         dir = (dir + 1) % 4
     else:
-        nx, ny = x + dx[dir], y + dy[dir]
+        x, y = x + dx[dir], y + dy[dir]
         break
 
-print(nx, ny)
+print(x, y)
