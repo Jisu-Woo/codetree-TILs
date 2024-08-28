86d6a1092ce1afd6819bdbc8c0018791680ed613
Jisu <162106530+Jisu-Woo@users.noreply.github.com>
2024-08-28 22:33:08 +0900

    Title: 모이자, Time: 120ms, Memory: 25MB, Status: Passed - CodeTree

--- 240828/모이자/gather.py
+++ 240828/모이자/gather.py
@@ -2,11 +2,13 @@ n = int(input())
 
 arr = list(map(int, input().split()))
 
-min = 1000
+min = 0
 for i in range(len(arr)):
     total = 0
     for j in range(len(arr)):
         total += arr[j] * abs(j - i)
+    if i == 0:
+        min = total
     if min > total:
         min = total
 
