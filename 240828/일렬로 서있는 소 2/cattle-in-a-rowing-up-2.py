225398fb3d87231cd660951ba06e38a3f8342371
Jisu <162106530+Jisu-Woo@users.noreply.github.com>
2024-08-28 22:26:00 +0900

    Title: 일렬로 서있는 소 2, Time: 125ms, Memory: 25MB, Status: Passed - CodeTree

--- 240828/일렬로 서있는 소 2/cattle-in-a-rowing-up-2.py
+++ 240828/일렬로 서있는 소 2/cattle-in-a-rowing-up-2.py
@@ -3,11 +3,10 @@ arr = list(map(int, input().split()))
 
 cnt = 0
 for i in range(len(arr)):
-    for j in range(len(arr)):
-        for k in range(len(arr)):
+    for j in range(i + 1, len(arr)):
+        for k in range(j + 1, len(arr)):
             if i < j and j < k:
                 if arr[i] <= arr[j] and arr[j] <= arr[k]:
                     cnt += 1
 
 print(cnt)
-$0
