6e7ff2fc558daf495f0c7e41dfa5a5be7d617c15
Jisu <162106530+Jisu-Woo@users.noreply.github.com>
2024-09-03 13:33:52 +0900

    Title: 마라톤 중간에 택시타기 2, Time: 119ms, Memory: 25MB, Status: Passed - CodeTree

--- 240903/마라톤 중간에 택시타기 2/taking-a-taxi-in-the-middle-of-the-marathon-2.py
+++ 240903/마라톤 중간에 택시타기 2/taking-a-taxi-in-the-middle-of-the-marathon-2.py
@@ -4,9 +4,9 @@ for i in range(n):
     line = list(map(int, input().split()))
     arr.append(line)
 
-min_dist = 100000
-for i in range(n):
-    if i == 0 or i == n - 1:
+min_dist = -1
+for i in range(n): # i 는 건너뛸 체크포인트의 인덱스
+    if i == 0 or i == n - 1: # 처음과 마지막은 안됨
         continue
     dist = 0
     j = 0
@@ -17,6 +17,8 @@ for i in range(n):
         else:
             dist += abs(arr[j + 1][0] - arr[j][0]) + abs(arr[j + 1][1] - arr[j][1])
             j += 1
+    if min_dist == -1: # 최소 거리 초기 값 할당해주기
+        min_dist = dist
     if dist < min_dist:
         min_dist = dist
 
