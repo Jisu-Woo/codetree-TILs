3e7c120e64a998a8ddc8bf25125ab2b52b6f8135
Jisu <162106530+Jisu-Woo@users.noreply.github.com>
2024-09-24 13:44:32 +0900

    Title: 배열 놀이, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240924/배열 놀이/play-with-array.py
@@ -0,0 +1,18 @@
+n, q = tuple(map(int, input().split()))
+
+arr = list(map(int, input().split()))
+
+# index 시작 번호 주의
+for i in range(q):
+    query = list(map(int, input().split()))
+    if query[0] == 1:
+        print(arr[query[1] - 1])
+    elif query[0] == 2:
+        idx = 0
+        idx = arr.index(query[1]) + 1
+        print(idx)
+    else:
+        string = ''
+        for elem in arr[query[1] - 1:query[2]]:
+            string += str(elem) + ' '
+        print(string)
