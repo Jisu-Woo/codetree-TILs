ac1863697fbd11ceeb8484d34a045b34bf9ed0fa
Jisu <162106530+Jisu-Woo@users.noreply.github.com>
2024-09-25 11:33:36 +0900

    Title: 배열 놀이, Time: 103ms, Memory: 24MB, Status: Passed - CodeTree

--- 240925/배열 놀이/play-with-array.py
+++ 240925/배열 놀이/play-with-array.py
@@ -8,8 +8,11 @@ for i in range(q):
         print(arr[query[1] - 1])
     elif query[0] == 2:
         idx = 0
-        idx = arr.index(query[1]) + 1
-        print(idx)
+        if query[1] not in arr: # 해당 값이 배열에 없는 경우 예외처리!!!
+            print(idx)
+        else:
+            idx = arr.index(query[1]) + 1
+            print(idx)
     elif query[0] == 3:
         str_ = ''
         for elem in arr[query[1]-1 : query[2]]:
